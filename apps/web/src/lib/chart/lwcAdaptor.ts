ca87835f93a5376247c1a546d3aac5e9c6feb930
Harshit Prakash <harshit99prakash@gmail.com>
2025-08-30 20:37:19 +0530

    Fixed adaptor issue

--- apps/web/src/lib/chart/lwcAdaptor.ts
+++ apps/web/src/lib/chart/lwcAdaptor.ts
@@ -1,16 +1,46 @@
-import { createChart, ISeriesApi } from "lightweight-charts";
+// src/lib/chart/lwcAdapter.ts   (rename from lwcAdaptor to avoid typos)
+import {
+  createChart,
+  CandlestickSeries,
+  HistogramSeries,
+  type IChartApi,
+  type UTCTimestamp,
+} from "lightweight-charts";
 
 export type OHLC = { t:number; o:number; h:number; l:number; c:number; v:number };
 
 export function mountLwc(el: HTMLDivElement) {
-  const chart = createChart(el, { autoSize: true, rightPriceScale: { borderVisible: false }, timeScale: { borderVisible: false } });
-  const candleSeries = chart.addCandlestickSeries();
-  const volumeSeries = chart.addHistogramSeries({ priceFormat: { type: 'volume' }, priceScaleId: '' });
-  chart.priceScale('').applyOptions({ scaleMargins: { top: 0.8, bottom: 0 } });
+  const chart: IChartApi = createChart(el, {
+    autoSize: true,
+    rightPriceScale: { borderVisible: false },
+    timeScale: { borderVisible: false },
+    layout: { fontFamily: "Inter, system-ui, sans-serif" },
+  });
+
+  // v5: addSeries(<SeriesDefinition>, <options>)
+  const candleSeries = chart.addSeries(CandlestickSeries, {
+    priceScaleId: "right",
+  });
+
+  const volumeSeries = chart.addSeries(HistogramSeries, {
+    priceScaleId: "left",
+    priceFormat: { type: "volume" },
+  });
+
+  chart.priceScale("left").applyOptions({
+    scaleMargins: { top: 0.8, bottom: 0 },
+  });
 
   function setData(bars: OHLC[]) {
-    const price = bars.map(b => ({ time: Math.floor(b.t/1000) as any, open: b.o, high: b.h, low: b.l, close: b.c }));
-    const vol   = bars.map(b => ({ time: Math.floor(b.t/1000) as any, value: b.v, color: (b.c >= b.o) ? undefined : undefined }));
+    const price = bars.map(b => ({
+      time: Math.floor(b.t / 1000) as UTCTimestamp,
+      open: b.o, high: b.h, low: b.l, close: b.c,
+    }));
+    const vol = bars.map(b => ({
+      time: Math.floor(b.t / 1000) as UTCTimestamp,
+      value: b.v,
+    }));
+
     candleSeries.setData(price);
     volumeSeries.setData(vol);
   }
